f9c1691a7d6643d5f07386001bf4823cc506fdc4
danielmortensen <daniel.t.mortensen@gmail.com>
2023-04-07 18:46:08 -0600

    applied comments per jake's feedback

--- test.m
+++ test.m
@@ -1,112 +1,67 @@
-% 2 bus 1 charger scenario
-nBus = 1; nCharger = 1;
-Var = util.getVarParam(nBus);
-Sim = util.getSimParam(nBus, nCharger);
-
-sol = nan([Var.nVar,1]);
-Sim.delta = zeros(size(Sim.delta));
-Sim(1,91) = 10;
-% no charging necessary
-for iBus = 1:Sim.nBus
-    for iTime = 1:Sim.nTime
-        sol(Var.b(iBus,iTime)) = 0;
-    end
+if ~exist('dataOpt','var')
+optimalPath = "\\wsl.localhost\ubuntu\home\dmortensen\paper4\results\LossType_fiscal_nBus_18_nCharger_6_nGroup_1_dt_20_minEnergy_0.00_proCost.mat";
+disoptimalPath = "\\wsl.localhost\ubuntu\home\dmortensen\paper4\results\LossType_fiscal_nBus_18_nCharger_6_nGroup_1_dt_20_minEnergy_0.00_proTime.mat";
+dataOpt = load(optimalPath);
+dataDis = load(disoptimalPath);
 end
 
-% initial state of charge
-for iBus = 1:Sim.nBus
-    sol(Var.h(iBus,1)) = Sim.eta(iBus);
-end
+pointOpt = getPoint(dataOpt);
+pointDis = getPoint(dataDis);
+figure; scatter(pointOpt(:,1),pointOpt(:,2),'blue');
+hold on; scatter(pointDis(:,1), pointDis(:,2),'red'); 
+xlabel('average charge rate'); ylabel('duration');
+legend('optimal','disoptimal'); shg
+set(gca,'xscale','log'); set(gca,'yscale','log');
+tabOpt = array2table(pointOpt,'VariableNames',{'avgPower','duration'});
+tabDis = array2table(pointDis,'VariableNames',{'avgPower','duration'});
+writetable(tabOpt,'\\wsl.localhost\ubuntu\home\dmortensen\paper4\documentation\media\11_results\avgPowerVsDurationOpt.csv');
+writetable(tabDis,'\\wsl.localhost\ubuntu\home\dmortensen\paper4\documentation\media\11_results\avgPowerVsDurationDis.csv');
+% isScheduledOpt = optimalData.Sim7.routeIdx;
+% scheduleOpt = optimalData.Sol7(optimalData.Var7.schedule);
+% nSessionOpt = optimalData.Sim7.nSession;
+% nBusOpt = optimalData.Sim7.nBus;
+% pointsOpt = zeros([sum(nSessionOpt),2]);
+% startOpt = optimalData.Sols5{1}.x(optimalData.Vars5{1}.sessStart);
+% finalOpt = optimalData.Sols5{1}.x(optimalData.Vars5{1}.sessFinal);
+% deltaTSec = optimalData.Sim1.deltaTSec;
+% iPoint = 1;
+% for iBus = 1:nBusOpt
+%     busSchedule = squeeze(scheduleOpt(iBus,:));
+%     for iSession = 1:nSessionOpt(iBus)
+%         timeIdx = isScheduledOpt(iBus,iSession);
+%         nTime = sum(timeIdx);
+%         mu = (timeIdx*busSchedule')/nTime;
+%         dt = nTime*deltaTSec;
+%         pointsOpt(iPoint,:) = [mu, dt];
+%     end
+% end
 
-% no change in state of charge
-for iBus = 1:Sim.nBus
-    for iTime = 2:Sim.nTime + 1
-        sol(Var.h(iBus,iTime)) = sol(Var.h(iBus,iTime - 1)) + Sim.delta(iBus,iTime - 1) +  sol(Var.pc(iBus,iTime));
-    end
-end
 
-% no charger use
-for iTime = 1:Sim.nTime
-    sol(Var.pc(iTime)) = 0;
-end
 
-% absolute valeu of g is still zero
+
+function point = getPoint(data)
+isScheduled = data.Sim7.routeIdx;
+schedule = data.Sol7.x(data.Var7.schedule);
+nSession = data.Sim7.nSession;
+nBus = data.Sim7.nBus;
+point = zeros([sum(nSession),2]);
+deltaTSec = data.Sim1.deltaTSec;
+iPoint = 1;
 for iBus = 1:nBus
-    for iTime = 1:Sim.nTime - 1
-        sol(Var.g(iBus,iTime)) = 0;
+    busSchedule = squeeze(schedule(iBus,:));
+    for iSession = 1:nSession(iBus)
+        timeIdx = logical(squeeze(isScheduled(iBus,iSession,:)));
+        val = busSchedule(timeIdx);
+        nTime = sum(val > 0);
+        mu = sum(val)/nTime;
+        dt = nTime*deltaTSec;
+        energy = mu*dt/3600;
+        
+        if energy > 1
+            point(iPoint,:) = [mu, dt];
+            iPoint = iPoint + 1;
+        end
     end
 end
-
-% compute facilities power
-f = max(Sim.u);
-
-% demand power
-d = max(Sim.u(Sim.S));
-sol(Var.facilities) = f;
-sol(Var.demand) = d;
-
-% on-peak energy
-eon = sum(Sim.u(Sim.S));
-eof = sum(Sim.u(~Sim.S));
-sol(Var.eOff) = eof;
-sol(Var.eOn) = eon;
-
-% calculate total power
-sol(Var.pt) = Sim.u;
-
-% set up constraints
-...Sim.delta = zeros(size(Sim.delta));
-...Sim.u = zeros(size(Sim.u));
-[A1, b1, nCon1, descr1, eq1] = con.getCon1(Sim,Var);
-[A2, b2, nCon2, descr2, eq2] = con.getCon2(Sim,Var);
-[A3, b3, nCon3, descr3, eq3] = con.getCon3(Sim,Var);
-[A4, b4, nCon4, descr4, eq4] = con.getCon4(Sim,Var);
-[A5, b5, nCon5, descr5, eq5] = con.getCon5(Sim,Var);
-[A6, b6, nCon6, descr6, eq6] = con.getCon6(Sim,Var);
-[A7, b7, nCon7, descr7, eq7] = con.getCon7(Sim,Var);
-[A8, b8, nCon8, descr8, eq8] = con.getCon8(Sim,Var);
-[A9, b9, nCon9, descr9, eq9] = con.getCon9(Sim,Var);
-[A10, b10, nCon10, descr10, eq10] = con.getCon10(Sim,Var);
-obj1 = con.getObj1(Sim, Var);
-obj2 = con.getObj2(Sim, Var);
-
-% plot first constraint
-b1sol = A1*sol;
-plotComparison(b1sol, b1);
-assertIn(b1sol, b1);
-
-% plot second constraint
-b2sol = A2*sol;
-plotComparison(b2sol,b2);
-assertEq(b2sol,b2);
-
-% plot third constraint
-b3sol = A3*sol;
-plotComparison(b3sol, b3);
-assertIn(b3sol,b3);
-
-% verify fourth constraint
-b4sol = A4*sol; 
-plotComparison(b4sol, b4);
-assertEq(b4sol,b4);
-
-% verify fifth constraint
-b5sol = A5*sol;
-plotComparison(b5sol, b5);
-assertEq(b5sol,b5);
-
-% verify sixth constraint
-b6sol = A6*sol;
-plotComparison(b6sol, b6);
-assertEq(b6sol, b6);
-
-
-function assertIn(d1, d2)
-    assert(sum(d1 > d2) == 0);
-end
-function assertEq(d1, d2)
-    assert(sum(d1 ~= d2) == 0);
-end
-function plotComparison(d1, d2)
-    figure; plot(d1); hold on; plot(d2); legend('given','expected');
+point = point(1:iPoint - 1,:);
 end
